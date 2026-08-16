c29ab4bf0bfe375d601d3c4734872ef4d16e33bf
tangmingtao <938206858@qq.com>
2026-08-16 12:49:35 +0800

    Add more ResutlCode to handle BusinessException

--- TMLibrary/src/main/java/com/tmt/TMLibrary/common/Result/ResultCode.java
+++ TMLibrary/src/main/java/com/tmt/TMLibrary/common/Result/ResultCode.java
@@ -3,8 +3,12 @@ package com.tmt.TMLibrary.common.Result;
 public enum ResultCode {
     SUCCESS(200, "成功"),
     BAD_REQUEST(400, "请求参数错误"),
+    UNAUTHORIZED(401,"未认证或认证失败"),
+    FORBIDDEN(403, "权限不足，禁止访问"),
     NOT_FOUND(404, "资源不存在"),
+    METHOD_NOT_ALLOWED(405, "请求方法不允许"),
     CONFLICT(409, "资源冲突"),
+    UNPROCESSABLE_ENTITY(422, "业务规则不满足"),
     INTERNAL_ERROR(500, "服务器内部错误");
 
     private final int code;
